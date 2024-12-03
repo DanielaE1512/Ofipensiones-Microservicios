f7a77281dcdba8dbed332aaf5c8fd6d52ac41bc0
aneira3 <a.neiras@uniandes.edu.co>
2024-12-03 13:28:53 -0500

    gestorEstudiante

---
+++ gestorEstudiante/src/main/java/com/example/gestorEstudiante/GestorEstudianteApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestorEstudiante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorEstudianteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorEstudianteApplication.class, args);
+	}
+
+}
---
+++ gestorEstudiante/src/test/java/com/example/gestorEstudiante/GestorEstudianteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestorEstudiante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorEstudianteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
